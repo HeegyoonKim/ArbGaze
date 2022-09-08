86b04a5ab9baaaac7fa575435862b7b3fc91cf8b
hgkim <khg941220@gmail.com>
2022-09-08 12:11:54 +0900

    Update train_teacher.bash

--- code/train_teacher.bash
+++ code/train_teacher.bash
@@ -1,12 +1,12 @@
 # UTMultiview dataset
 for f in $(seq 1 1 3);
 do
-    python main.py --dataset_name utmv --fold $f --save_path ../results/utmv/teacherf${f} \
+    python main.py --dataset_name utmv --fold $f --save_path ../results/utmv/HR/f${f} \
                    --train_img_type HR --test_img_type HR
 done
 # MPIIGaze dataset
 for f in $(seq 0 1 14);
 do
-    python main.py --dataset_name mpii --fold $f --save_path ../results/mpii/teacherf${f} \
+    python main.py --dataset_name mpii --fold $f --save_path ../results/mpii/HR/f${f} \
                    --train_img_type HR --test_img_type HR
 done
